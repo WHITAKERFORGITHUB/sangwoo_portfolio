ec45a11aae48565e90444da777f3ccb480931fc8
WHITAKERFORGITHUB <hun1246@hanmail.net>
2025-07-11 22:58:25 +0900

    9.7 홈화면 투명하게 처리 까지

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-const header = document.querySelector('.header')
-
 // 헤더의 높이를 알아내보자
+const header = document.querySelector('.header')
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener('scroll', function() {
     //console.log('헤더높이: ' + scrollY)
@@ -9,5 +8,14 @@ document.addEventListener('scroll', function() {
     } else {
         header.classList.remove('header--dark')
     }
+})
 
+// y좌표의 높이가 home끝에 가까워질수록 투명하게
+// Home 섹션을 아래로 스크롤 시 투명하게 처리
+const home = document.querySelector('.home__container')
+const homeHeight = home.offsetHeight;
+console.log(homeHeight)
+document.addEventListener('scroll', function() {
+    //console.log(1 - window.scrollY / homeHeight)
+    home.style.opacity = 1 - window.scrollY / homeHeight
 })
