1842cfc2d91c65539f747063df2a9a9ff28202ae
WHITAKERFORGITHUB <hun1246@hanmail.net>
2025-07-16 00:14:54 +0900

    10.자바스크립트 구현(심화) 끝까지

--- src/main.js
+++ src/main.js
@@ -42,3 +42,78 @@ navbarToggle.addEventListener('click', ()=> {
 navbarMenu.addEventListener('click', ()=> {
     navbarMenu.classList.remove('open')
 })
+
+
+
+// 구현계획
+// 1. 모든섹션 요소들과 메뉴 아이템들을 가지고 온다.
+// 2. IntersectionObserver를 사용해서 모든 섹션들을 관찰해야 한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+// 보여지는 섹션:
+//   - 다수의 섹션이 동시에보여진다면, 가장 첫번째 섹션을 선택
+//   - 마지막 contact 섹션이 보여진다면, 가장 마지막 섹션을 선택
+const sectionIds = ['#home', '#about', '#skills', '#testimonial', '#contact'];
+const sections = sectionIds.map(id=>document.querySelector(id))
+const navItems = sectionIds.map(id => document.querySelector(`[href="${id}"]`))
+//console.log(navItems)
+//console.log(sectionIds)
+//console.log(sections)
+
+const visibleSections = sectionIds.map(()=> {
+    false;
+})
+
+// 10.10 메뉴선택하기
+let activeNavItem = navItems[0];
+
+const options = {
+    rootMargin: '-20% 0px 0px 0px',
+    threshold: [0, 0.98]
+};
+const observer = new IntersectionObserver(observerCallback, options);
+//sections.forEach(section => observer.observe(section));
+sections.forEach((section)=> {
+    observer.observe(section)
+})
+
+function observerCallback(entries) {
+
+    // flag변수
+    let selectLastOne;
+
+    entries.forEach((entry)=> {
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        visibleSections[index] = entry.isIntersecting;
+        selectLastOne = 
+            index === sectionIds.length - 1 && 
+            entry.isIntersecting && 
+            entry.intersectionRatio >= 0.95
+            
+        //console.log(entry.target.id)
+        //console.log(index)
+        //console.log(entry.target)
+        //console.log(entry.isIntersecting)
+        //console.log(entry.intersectionRatio)
+    })
+    //console.log(visibleSections)
+    //console.log(selectLastOne);
+
+    const navIndex = selectLastOne ? sectionIds.length - 1: findFirstIntersecting(visibleSections);
+    //console.log(sectionIds[navIndex])
+
+    // 10.10 메뉴선택하기   
+    selectNavItem(navIndex);
+}
+
+function findFirstIntersecting(intersections) {
+    const index = intersections.indexOf(true);
+    return index >= 0 ? index : 0
+}
+
+function selectNavItem(index) {
+    const navItem = navItems[index];
+    if(!navItem) return;
+    activeNavItem.classList.remove('active');
+    activeNavItem = navItem;
+    activeNavItem.classList.add('active');
+}
